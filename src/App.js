caaa270766844957673d1ec9a1d1922bbd356d92
Uttam1728 <ushankradadiya1728@gmail.com>
2021-03-27 23:58:09 +0530

    App js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NotificationForm from './components/notificationform';
+import ShowDate from './components/showdate';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { datetime: null };
+  }
+  render() {
+    return (
+      <div >
+        <div class="row">
+          <div class="col-sm-1">
+           
+           </div>
+          <div class="col-sm-5">
+            {/* Reusable Notification form take Date and time individualy and return them to parent */ }
+            <NotificationForm onChange={ datetime => { this.setState({ datetime }); console.log(datetime) } } />
+
+          </div>
+          <div class="col-sm-5">
+            {/* Reusable ShowDate that take Datetime as i/p and displays it in appropriateformet */ }
+            <ShowDate datetime={ this.state.datetime } />
+
+          </div>
+          <div class="col-sm-1">
+           
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
